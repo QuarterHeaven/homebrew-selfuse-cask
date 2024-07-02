3c9ed3cd52409112931b9c63ef482b294019cfa4
QuarterHeaven <liaotx2@gmail.com>
2024-07-02 17:31:25 +0800

    update squirrel

--- Casks/s/squirrel.rb
+++ Casks/s/squirrel.rb
@@ -1,28 +1,28 @@
 cask "squirrel" do
-  version "1.0.2"
-  sha256 "2912bf3435d5c85aa87b0eaa3f08a487888b8bbbe3b48ea0ccef1a5f8215cebf"
+  version "0.16.2"
+  sha256 "e08d28fd72445bccbdbccc06b16a9e300c07371f67d576cd4ed35731be9d4ad6"
 
-  url "https://github.com/rime/squirrel/releases/download/#{version}/Squirrel-#{version}.pkg",
+  url "https://github.com/rime/squirrel/releases/download/#{version}/Squirrel-#{version}.zip",
       verified: "github.com/rime/squirrel/"
   name "Squirrel"
   desc "Rime input method engine"
   homepage "https://rime.im/"
 
   auto_updates true
-  depends_on macos: ">= :ventura"
 
-  pkg "Squirrel-#{version}.pkg"
+  pkg "Squirrel.pkg"
 
   uninstall pkgutil: [
-              "com.googlecode.rimeime.Squirrel.pkg", # Package name of older versions (< 0.10.0)
               "im.rime.inputmethod.Squirrel",
+              "com.googlecode.rimeime.Squirrel.pkg", # Package name of older versions (< 0.10.0)
             ],
             delete:  "/Library/Input Methods/Squirrel.app"
 
   zap trash: [
-    "~/Library/Caches/com.googlecode.rimeime.inputmethod.Squirrel",
     "~/Library/Caches/im.rime.inputmethod.Squirrel",
-    "~/Library/Preferences/com.googlecode.rimeime.inputmethod.Squirrel.plist",
     "~/Library/Preferences/im.rime.inputmethod.Squirrel.plist",
+    # Data for older versions (< 0.10.0)
+    "~/Library/Caches/com.googlecode.rimeime.inputmethod.Squirrel",
+    "~/Library/Preferences/com.googlecode.rimeime.inputmethod.Squirrel.plist",
   ]
 end
